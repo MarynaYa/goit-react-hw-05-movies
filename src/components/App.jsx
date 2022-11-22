3972e3df7005f18763496fba318fabd18ece1a37
MarynaYa <103567926+MarynaYa@users.noreply.github.com>
2022-11-22 20:09:15 +0200

    second-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
+import { lazy, Suspense } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AppBar from './AppBar/AppBar';
+//import Navigation from './Navigation/Navigation';
+import Container from './Container/Container';
+import Loader from './Loader/Loader';
+
+const HomePage = lazy(() =>
+  import('../pages/HomePage/HomePage' /* webpackChunkName: "Home-page" */),
+);
+const MoviesPage = lazy(() =>
+  import('../pages/MoviesPage/MoviePage' /* webpackChunkName: "Movies-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    '' /* webpackChunkName: "Movie-details-page" */
+  ),
+);
+
+function App() {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+    <AppBar />
+
+    <Suspense fallback={<Loader />}>
+     
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+
+        <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
+
+    </Suspense>
+  </Container>
   );
 };
+
+export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,13 +5,19 @@ import s from './Navigation.module.css';
 export default function Navigation() {
   return (
     <nav className={s.nav}>
+        <li>
       <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>
         Home
       </NavLink>
+      </li>
 
+      <li>
       <NavLink to="/movies" className={s.link} activeClassName={s.activeLink}>
         Movies
       </NavLink>
+      </li>
+      
     </nav>
+
   );
 }
